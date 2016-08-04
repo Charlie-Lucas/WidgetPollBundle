004b4d449eabb61f8e442a6818c70c9145e0ce0d
Grégoire Humeau <gregoire@appventus.com>
2016-08-04 18:09:06 +0200

    Add configuration for victoire menu item

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,11 +19,13 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('victoire_widget_poll');
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
+        $rootNode
+            ->children()
+                ->booleanNode('victoire_menu_item')
+                    ->defaultFalse()
+                ->end()
+            ->end();
+        
         return $treeBuilder;
     }
 }
